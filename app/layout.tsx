adb87e24698e6913cb19fe7a94c9b995838402a5
sunsky89757 <113254524+sunsky89757@users.noreply.github.com>
2024-09-11 08:40:59 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "GongxiangAI",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "GongxiangAI",
     statusBarStyle: "default",
   },
 };
